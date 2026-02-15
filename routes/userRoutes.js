ac7f61c7b8830a680f343e805728284012a6724c
Aditya Rana <aditya.rana52@gmail.com>
2026-02-15 17:13:46 +0530

    Added profile route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,12 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.put("/subscribe", protect, activateSubscription);
 
+router.get("/profile", protect, async (req, res) => {
+  res.json({
+    _id: req.user._id,
+    mobile: req.user.mobile,
+    subscriptionActive: req.user.subscriptionActive,
+  });
+});
+
 module.exports = router;
