afeb67828872f21b2c61d25fe5056a37f7357f78
Aditya Rana <aditya.rana52@gmail.com>
2026-02-15 20:27:33 +0530

    Fixed protect import in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { activateSubscription } = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
+const protect = require("../middleware/authMiddleware");
+
 
 router.put("/subscribe", protect, activateSubscription);
 
