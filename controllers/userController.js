3e69db4277c6dae3e12564822eb3082402504518
Aditya Rana <aditya.rana52@gmail.com>
2026-02-19 21:03:12 +0530

    Added subscription expiry logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,12 +8,19 @@ exports.activateSubscription = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // 1 year validity
+    const oneYearLater = new Date();
+    oneYearLater.setFullYear(oneYearLater.getFullYear() + 1);
+
     user.subscriptionActive = true;
-    await user.save();
+    user.subscriptionExpiresAt = oneYearLater;
+
+    await user.save()
 
     res.json({
       message: "Subscription activated",
       subscriptionActive: user.subscriptionActive,
+      expiresAt: oneYearLater,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
