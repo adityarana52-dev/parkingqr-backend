cbe8eeb016c382e5c9c937292a408ab2e8cf5002
Aditya Rana <aditya.rana52@gmail.com>
2026-02-19 20:34:04 +0530

    Added subscription expiry field

--- models/User.js
+++ models/User.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+
+    subscriptionExpiresAt: {
+        type: Date,
+      },
+      
     expoPushToken: {
   type: String,
 },
