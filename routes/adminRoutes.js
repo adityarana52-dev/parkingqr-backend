3db09a1b5c41d30bb4a269e3c60d0ef8c80d1fe5
Aditya Rana <aditya.rana52@gmail.com>
2026-03-24 17:04:43 +0530

    Start

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -383,10 +383,11 @@ router.get("/download-showroom-qr/:showroomId", async (req, res) => {
     </html>
     `;
 
-    const browser = await puppeteer.launch({
+   const browser = await puppeteer.launch({
+      executablePath: "/usr/bin/google-chrome-stable",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
-
+s
     const page = await browser.newPage();
 
     await page.setContent(html, { waitUntil: "networkidle0" });
