fc4e3932bc1b3e8432145c8891c316e67553a88a
Aditya Rana <aditya.rana52@gmail.com>
2026-03-27 15:15:49 +0530

    start

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -267,11 +267,6 @@ router.get("/download-showroom-qr/:showroomId", async (req, res) => {
   // 👉 SAME QR 2 TIMES
   for (let copy = 0; copy < 2; copy++) {
 
-    doc
-  .lineWidth(0.5)
-  .strokeColor("#999")
-  .rect(x, y, cardWidth, cardHeight)
-  .stroke();
 
     // TEMPLATE
     doc.image(templatePath, x, y -40, {
