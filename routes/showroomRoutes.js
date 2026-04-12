20cad8a9de52cde0e957d04687045d8126add810
Aditya Rana <aditya.rana52@gmail.com>
2026-04-12 15:06:20 +0530

    start

--- routes/showroomRoutes.js
+++ routes/showroomRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const Showroom = require("../models/Showroom");
 const SalesPerson = require("../models/SalesPerson");
 const QrCode = require("../models/QrCode");
