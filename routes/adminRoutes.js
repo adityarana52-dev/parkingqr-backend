24abe536e7a392aec154cb4673f4e3798da39f86
Aditya Rana <aditya.rana52@gmail.com>
2026-04-12 14:17:07 +0530

    start

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -428,7 +428,7 @@ router.get("/download-showroom-qr/:showroomId", async (req, res) => {
           // QR (same as before)
           const qrSize = 120;
           const qrX = x + (cardWidth - qrSize) / 2;
-          const qrY = y + 22;
+          const qrY = y + 20;
 
           doc.image(qrBuffer, qrX, qrY, {
             width: qrSize,
